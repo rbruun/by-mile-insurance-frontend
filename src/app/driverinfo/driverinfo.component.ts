a30970b4a2cec83bae1ef20521285473794ad0e8
Tate n0112138 <Terri.Tate@LibertyMutual.com>
2017-07-21 09:32:18 -0400

    style vehinfo and tripinfo pages

--- src/app/driverinfo/driverinfo.component.ts
+++ src/app/driverinfo/driverinfo.component.ts
@@ -1,6 +1,6 @@
 
 import 'rxjs/add/operator/switchMap';
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params, Router } from '@angular/router';
 import { Location } from '@angular/common';
 import { NgForm } from '@angular/forms';
@@ -15,15 +15,11 @@ import { QuoteInfoService } from '../quote-info.service';
 
 export class DriverinfoComponent implements OnInit {
 
-  studentForm: NgForm;
-  @ViewChild('driverForm') currentForm: NgForm;
-
   quoteId: string;
 
   driver =
-
   {
-    quote: {quoteID: <string>null},
+    quote: {quoteId: <string>null},
     firstName: <string>null,
     last_name: <string>null,
     addressLine1: <string>null,
@@ -32,7 +28,7 @@ export class DriverinfoComponent implements OnInit {
     state: <string>null,
     zip_code: <number>null,
     gender: <string>null,
-    birthDate: <number>null,
+    birthDate: <string>null,
     maritalStatus: <string>null,
     education: <string>null,
     homeOwnerStatus: <string>null,
@@ -50,7 +46,7 @@ export class DriverinfoComponent implements OnInit {
     this.route.params.subscribe(
       (params: Params) => {
         this.quoteId = params['quoteId'];
-        this.driver.quote.quoteID = this.quoteId;
+        this.driver.quote.quoteId = this.quoteId;
         console.log(this.quoteId);
       }
     );
@@ -58,7 +54,9 @@ export class DriverinfoComponent implements OnInit {
 
     saveDriver() {
       console.log(this.driver)
-      this.quoteInfoService.addRecord('addDriver', this.driver).subscribe()
+      this.quoteInfoService.addRecord('addDriver', this.driver).subscribe();
+      console.log("after API call");
       this.router.navigate(['vehinfo', this.quoteId])
+      console.log("after pass");
     }
 };
