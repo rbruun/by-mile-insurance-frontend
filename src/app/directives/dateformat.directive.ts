4b46a43dd34385795efeead5e71fb9be5ae26895
Ron Bruun <ronald.bruun@libertymutual.com>
2017-07-25 17:17:00 -0400

    fix backspace

--- src/app/directives/dateformat.directive.ts
+++ src/app/directives/dateformat.directive.ts
@@ -10,19 +10,16 @@ export class DateformatDirective {
 
   constructor(private el: ElementRef) { }
 
-  @HostListener('keyup') onKeyUp() {
-    // var numChars = $scope.date.length;
-		// 		if(numChars === 2 || numChars === 5){
-		// 			var thisVal = $scope.date;
-		// 			thisVal += '/';
-		// 			$scope.date = thisVal;
-		// 		}
+  @HostListener('keyup', ['$event']) onKeyUp(e: KeyboardEvent) {
     var numChars = this.el.nativeElement.value.length;
     	if(numChars === 4 || numChars === 7){
 				var thisVal = this.el.nativeElement.value;
 					thisVal += '-';
 					this.el.nativeElement.value = thisVal;
 				}
-  }
+			if ((numChars == 5 || numChars == 8) && e.key == "Backspace") {
+					this.el.nativeElement.value = this.el.nativeElement.value.slice(0,-1);
+			}
+	}
 
 }
