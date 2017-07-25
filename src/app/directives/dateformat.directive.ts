72db7cb92cdfd661337fa9be765ca4d5f3e89695
Ron Bruun <ronald.bruun@libertymutual.com>
2017-07-25 16:19:29 -0400

    add directive

--- src/app/directives/dateformat.directive.ts
+++ src/app/directives/dateformat.directive.ts
@@ -11,7 +11,18 @@ export class DateformatDirective {
   constructor(private el: ElementRef) { }
 
   @HostListener('keyup') onKeyUp() {
-    console.log("keyup hit");
+    // var numChars = $scope.date.length;
+		// 		if(numChars === 2 || numChars === 5){
+		// 			var thisVal = $scope.date;
+		// 			thisVal += '/';
+		// 			$scope.date = thisVal;
+		// 		}
+    var numChars = this.el.nativeElement.value.length;
+    	if(numChars === 4 || numChars === 7){
+				var thisVal = this.el.nativeElement.value;
+					thisVal += '-';
+					this.el.nativeElement.value = thisVal;
+				}
   }
 
 }
