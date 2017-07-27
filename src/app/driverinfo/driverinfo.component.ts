86388a59613b7b65c06688c0aebbc880ad4a3097
Tate n0112138 <Terri.Tate@LibertyMutual.com>
2017-07-27 09:08:52 -0400

    add multi drivers and continue to style

--- src/app/driverinfo/driverinfo.component.ts
+++ src/app/driverinfo/driverinfo.component.ts
@@ -20,8 +20,9 @@ export class DriverinfoComponent implements OnInit {
   driver =
   {
     quote: {quoteId: <string>null},
+    driver: {driverId: <string>null},
     firstName: <string>null,
-    last_name: <string>null,
+    lastName: <string>null,
     addressLine1: <string>null,
     addressLine2: <string>null,
     city: <string>null,
@@ -33,6 +34,16 @@ export class DriverinfoComponent implements OnInit {
     education: <string>null,
     homeOwnerStatus: <string>null,
   }
+
+  drivers = [];
+  driversTotal = {
+    quote: {quoteId: <string> null},
+    driver: {driverId: <string> null}
+  }
+
+  errorMessage: string;
+  successMesssage: string;
+
 
   constructor(
     private quoteInfoService: QuoteInfoService,
@@ -52,11 +63,63 @@ export class DriverinfoComponent implements OnInit {
     );
   }
 
-    saveDriver() {
-      console.log(this.driver)
-      this.quoteInfoService.addRecord('addDriver', this.driver).subscribe();
-      console.log("after API call");
-      this.router.navigate(['vehinfo', this.quoteId])
-      console.log("after pass");
-    }
+//     saveDriver() {
+//       console.log(this.driver)
+//       this.quoteInfoService.addRecord('addDriver', this.driver).subscribe();
+//       console.log("after API call");
+//       this.router.navigate(['vehinfo', this.quoteId])
+//       console.log("after pass");
+//     }
+// };
+
+
+
+addDriver() {
+  // call the data service to add trip
+  this.quoteInfoService.addRecord('addDriver', this.driver).subscribe(
+    driver => this.getDrivers()
+  );
+
+  this.driver.firstName = null;
+  this.driver.lastName = null;
+  this.driver.addressLine1 = null;
+  this.driver.addressLine2 = null;
+  this.driver.city = null;
+  this.driver.state = null;
+  this.driver.zip_code = null;
+  this.driver.gender = null;
+  this.driver.birthDate = null;
+  this.driver.maritalStatus = null;
+  this.driver.education = null;
+  this.driver.homeOwnerStatus = null;
+
+}
+
+getDrivers() {
+console.log("getDrivers");
+  this.quoteInfoService.getRecords("getDrivers", this.quoteId)
+    .subscribe(
+      drivers => {this.drivers = drivers},
+      error =>  this.errorMessage = <any>error);
+}
+
+deleteDriver(id: number) {
+      this.quoteInfoService.deleteRecord("deleteDriver", id)
+        .subscribe(
+          trip => {this.successMesssage = "Record(s) deleted succesfully";
+                  console.log("record deleted");this.getDrivers(); },
+          error =>  console.log(error));
+}
+
+goToVehicle() {
+  this.driversTotal.quote.quoteId = this.quoteId;
+  // this.driversTotal.driver.driverId = this.driverId;
+  // call the data service to add trip totals
+  // this.quoteInfoService.addRecord('addDriver', this.driversTotal).subscribe();
+
+  // route to the summary page
+  this.router.navigate(['vehinfo', this.quoteId]);
+}
+
+
 };
