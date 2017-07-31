0b27c3c825a14a7b961f4a05a8784bb7923b227d
Ron Bruun <ronald.bruun@libertymutual.com>
2017-07-31 16:32:26 -0400

    fix navigation if page is refreshed

--- src/app/navigation.service.ts
+++ src/app/navigation.service.ts
@@ -4,16 +4,21 @@ import { Subject }    from 'rxjs/Subject';
 @Injectable()
 export class NavigationService {
 
+    private homeAnnouncedSource = new Subject<string>();
     private driverAnnouncedSource = new Subject<string>();
     private vehicleAnnoucedSource = new Subject<string>();
     private tripAnnoucedSource = new Subject<string>();
     private summaryAnnoucedSource = new Subject<string>();
 
+  homeAnnounced$ = this.homeAnnouncedSource.asObservable();
   driverAnnounced$ = this.driverAnnouncedSource.asObservable();
   vehicleAnnounced$ = this.vehicleAnnoucedSource.asObservable();
   tripAnnounced$ = this.tripAnnoucedSource.asObservable();
   summaryAnnounced$ = this.summaryAnnoucedSource.asObservable();
 
+  announceHome() {
+    this.homeAnnouncedSource.next(null);
+  }   
   announceDriver() {
     this.driverAnnouncedSource.next(null);
   }    
